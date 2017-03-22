4b4ca1c921e7c0eaa25ca88a1de38aa4b915509f
Himanshu Nachane <himanshu.n1495@gmail.com>
2017-03-22 16:23:23 +0530

    Citizen login completed.Minute changes made to citizen signup

--- citizen_login.php
+++ citizen_login.php
@@ -1,32 +1,27 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
+include "connection.php";
 
-	if(!isset($_POST["uname"])&& !isset($_POST["password"])){ echo "Nothing is set";}
-	else{//check input or if it is set 
-		if(empty($_POST["uname"	]) && empty($_POST["password"])) echo "Empty";//to check empty
-		else{
-			$email = $_POST["uname"];// username
-			$password = $_POST["password"];// password
-			//login check
-			include "config.php";// include connection 
-			$loginCheckQry = "select * from `citizen_data` where `uname` ='".$uname."' and `password` = '".$password."'";//check query
-			if(!$loginCheck = mysqli_query($con,$loginCheckQry)){// get query data and fire on connection
-				echo "".mysqli_error($con);//display error
-				mysqli_close($con);// close connection
-			}
-			else if(mysqli_num_rows($loginCheck)==0) {//check number of rows in login check
-				mysqli_close($con);//close connection
-				echo 'Did you had correct credentials? Try again.';
-			}
-			else{
-				//session start
- 				$getUser = mysqli_fetch_assoc($loginCheck);//to fetch the all rows in array  
-				mysqli_close($con);
-				session_start();
-				$_SESSION["id"] = $getUser["uname"];// set a session
-				//$_SESSION["email"] = $getUser["email"];
-				echo $_SESSION["id"];
-			}
-		}	
+$email = $_POST['uemail'];
+$password = $_POST['pass'];
+$flag = false;
+$check = mysqli_query($connect,"SELECT password FROM citizen_data WHERE email='$email'");
+if (mysqli_affected_rows($connect)==1) {
+	while ($row=mysqli_fetch_assoc($check)) {
+		$to_validate = $row['password'];
 	}
+	if (password_verify($password,$to_validate)) {
+		$result = mysqli_query($connect,"SELECT * FROM citizen_data WHERE email='$email' AND password='$to_validate'");
+		if (mysqli_num_rows($result)==1) {
+			$flag = true;
+			echo "done";							
+		}			
+	}		
+}
+
+if ($flag==false) {
+	echo "invalid";
+}
+		
+			 
 ?>
