19bc1aca4e7f078b4ad72471caf737f02e141443
Himanshu Nachane <himanshu.n1495@gmail.com>
2017-03-22 15:09:01 +0530

    Validation and AJAX fixed. Code is working.

--- citi_signup.php
+++ citi_signup.php
@@ -1,33 +1,33 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-require_once("connection.php");
+include "connection.php";
 
-$name = $_POST['uname'];
 $email = $_POST['uemail'];
 $password = $_POST['confpass'];
 $zipcode = $_POST['zipcode'];
 $contact = $_POST['contact'];
+$email_flag = false;
+$contact_flag = false;
 
 
+$validZipcode = $validContact = "";
 
-$validName = $validZipcode = $validContact = "";
-
-$result = mysqli_query($connect,"SELECT username FROM citizen_data WHERE username='$name'");
-if (mysqli_num_rows($result)!=0) {
-	echo "username";	
+if ($email_flag == false && $contact_flag == false) {
+	$result = mysqli_query($connect,"SELECT email FROM citizen_data WHERE email='$email'");
+	if (mysqli_num_rows($result)!=0) {
+		echo "email";
+		$email_flag = true;
+	}
 }
-$result = mysqli_query($connect,"SELECT email FROM citizen_data WHERE email='$email'");
-if (mysqli_num_rows($result)!=0) {
-	echo "email";
+if ($email_flag == false && $contact_flag == false) {
+	$result = mysqli_query($connect,"SELECT contact FROM citizen_data WHERE contact='$contact'");
+	if (mysqli_num_rows($result)!=0) {
+		echo "contact";
+		$contact_flag = true;
+	}
 }
-$result = mysqli_query($connect,"SELECT contact FROM citizen_data WHERE contact='$contact'");
-if (mysqli_num_rows($result)!=0) {
-	echo "contact";
-}
-else{
+if ($email_flag == false && $contact_flag == false){
 
-$name = validate($name);
-$validName = true;
 $zipcode = validate($zipcode);
 $validZipcode = true;
 $contact = validate($contact);
@@ -36,10 +36,10 @@ $password = password_hash($password,PASSWORD_DEFAULT);
 
 
 
-if ($validName == true && $validZipcode == true && $validContact == true) {
-	$result = mysqli_query($connect,"INSERT INTO citizen_data(username,email,password,zipcode,contact) VALUES('$name','$email','$password','$zipcode','$contact')");
+if ($validZipcode == true && $validContact == true) {
+	$result = mysqli_query($connect,"INSERT INTO citizen_data(email,password,zipcode,contact) VALUES('$email','$password','$zipcode','$contact')");
 	if ($result){
-		echo "Updated";
+		echo "done";
 	}
 	else {
 		echo "Error: ". mysqli_error($connect);
